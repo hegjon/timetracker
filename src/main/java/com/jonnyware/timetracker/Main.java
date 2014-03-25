bcfdcba4f37cb7adb2a538f2eb4a46438a399941
Jonny Heggheim <hegjon@gmail.com>
2014-03-25 18:31:08 +0100

    Include the total hours.

--- src/main/java/com/jonnyware/timetracker/Main.java
+++ src/main/java/com/jonnyware/timetracker/Main.java
@@ -14,19 +14,23 @@ public class Main {
         File file = new File(args[0]);
         YearParser parser = new YearParser(new FileInputStream(file));
         System.out.println("Year: " + parser.getYear());
-        System.out.println("===========");
+        System.out.println("-------------");
 
         Collection<Interval> entries = parser.listTimeEntries();
         IntervalGroupBy groupBy = new IntervalGroupBy(entries);
 
+        Duration total = new Duration(0);
         for (Map.Entry<Integer, Collection<Interval>> week : groupBy.weekOfYear().entrySet()) {
-            Duration d = new Duration(0);
+            Duration totalPerWeek = new Duration(0);
             for (Interval interval : week.getValue()) {
-                d = d.plus(interval.toDuration());
+                totalPerWeek = totalPerWeek.plus(interval.toDuration());
+                total = total.plus(interval.toDuration());
             }
 
 
-            System.out.println("Week " + week.getKey() + ":\t" + d.getStandardHours() + "h");
+            System.out.println("Week " + week.getKey() + ":\t " + totalPerWeek.getStandardHours() + "h");
         }
+        System.out.println("-------------");
+        System.out.println("Total:\t " + total.getStandardHours() + "h");
     }
 }
